21ec991d585b794e57bc5fce411e22ec4c08e75b
ankur0578 <ankurchauhan7302@gmail.com>
2024-07-23 00:53:01 +0530

    monotonic array

--- src/Array/MonotonicArray.java
+++ src/Array/MonotonicArray.java
@@ -1,2 +1,24 @@
-package Array;public class MonotonicArray {
+package Array;
+
+public class MonotonicArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,3};
+        boolean res = isMonotonic(nums);
+        System.out.println(res);
+    }
+    public static boolean isMonotonic(int[] nums) {
+        boolean increasing = true;
+        boolean decreasing = true;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
+                decreasing = false; // If any element is greater than the previous, it's not decreasing
+            }
+            if (nums[i] < nums[i - 1]) {
+                increasing = false; // If any element is smaller than the previous, it's not increasing
+            }
+        }
+
+        return increasing || decreasing;
+    }
 }
